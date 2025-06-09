b63f4a40038d2a37f54214a1a53a77e20a1c558b
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-06-10 00:02:01 +0600

    Change 51

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,11 +1,17 @@
-﻿namespace CatalogAPI.Products.CreateProduct
-{
-    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
-  
-    public record CreateProductResponse(Guid Id);
-      
-    public class CreateProductEndpoint
-    {
+﻿using Carter;
 
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);  
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
 }
