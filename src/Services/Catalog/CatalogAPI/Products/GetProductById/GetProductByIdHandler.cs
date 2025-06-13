6c4511e83150656b37c4b73101ca8a3eb69a4153
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-06-14 02:15:19 +0600

    Change 71 Checking Get product by id api

--- src/Services/Catalog/CatalogAPI/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProductById/GetProductByIdHandler.cs
@@ -10,7 +10,7 @@
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
             logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
-
+             
             var product = await session.LoadAsync<Product>(query.Id,cancellationToken);
 
             if(product is null)
