164cb46b406e965bdb77e273af3225835dd38b14
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-06-21 21:10:16 +0600

    Change 78

---
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace CatalogAPI.Products.DeleteProduct
+{
+
+    public record DeleteProductCommand(Guid Id): ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+
+
+    internal class DeleteProductCommandHandler
+        (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand Command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Command}", Command);
+
+            session.Delete<Product>(Command.Id);
+            await session.SaveChangesAsync();
+
+            return new DeleteProductResult(true);
+
+        }
+    }
+
+
+}
