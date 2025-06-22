62b4a86d3a8c1b4dd9cec0ecb2f338e48e6f5489
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-06-23 01:58:43 +0600

    Change 88

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,5 +1,7 @@
 
 using BuildingBlocks.Behavior;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,5 +27,33 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context =>
+    {
+        
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if(exception == null)
+        {
+            return;
+        }
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Detail = exception.StackTrace,
+            Status = StatusCodes.Status500InternalServerError
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+
+        await context.Response.WriteAsJsonAsync(problemDetails);
+
+    });
+});
+
 
 app.Run();
