22b0b9808cbf11581796aa0aa64145e5f630f8bb
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-06-23 00:18:08 +0600

    Change 84

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
