87e5303c75b89f3e5fd8c650c39c60beadbcbb6b
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-07-25 15:22:16 +0600

    Change 94

--- src/Services/Catalog/CatalogAPI/GlobalUsing.cs
+++ src/Services/Catalog/CatalogAPI/GlobalUsing.cs
@@ -1,11 +1,12 @@
-﻿global using Carter;
-global using Mapster;
-global using MediatR;
-global using Marten;
+﻿global using BuildingBlocks.Behavior;
 global using BuildingBlocks.CQRS;
-global using CatalogAPI.Models;
-global using CatalogAPI.Exceptions;
-global using FluentValidation;
-global using BuildingBlocks.Behavior;
 global using BuildingBlocks.Exceptions.Handler;
+global using Carter;
+global using CatalogAPI.Exceptions;
+global using CatalogAPI.Models;
+global using FluentValidation;
+global using Mapster;
+global using Marten;
+global using MediatR;
+global using CatalogAPI.Data;
 
