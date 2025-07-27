e6aea04f5ff01a2646484f46ea260c17191c0b12
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-07-27 23:30:03 +0600

    Change 97

--- src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductsHandler.cs
@@ -1,8 +1,6 @@
-﻿
-
-namespace CatalogAPI.Products.GetProducts
+﻿namespace CatalogAPI.Products.GetProducts
 {
-    public record GetProductsQuery(): IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
 
 
@@ -12,7 +10,8 @@ namespace CatalogAPI.Products.GetProducts
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
 
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
         
             return new GetProductsResult(products);
         
