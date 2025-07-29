3373a09020e5f8738e8be00002443bac49c57f81
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-07-29 19:01:19 +0600

    change 106

--- src/Services/Basket/BasketAPI/Program.cs
+++ src/Services/Basket/BasketAPI/Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
