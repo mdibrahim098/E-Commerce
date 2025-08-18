fd5b8925b51b3eba21b5b0bccdd804aa171c91ce
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-08-19 00:15:08 +0600

    Change 114

--- src/Services/Basket/BasketAPI/Basket/DeleteBasket/DeleteBasketEndpoints.cs
+++ src/Services/Basket/BasketAPI/Basket/DeleteBasket/DeleteBasketEndpoints.cs
@@ -1,6 +1,35 @@
-﻿namespace BasketAPI.Basket.DeleteBasket
+﻿
+using BasketAPI.Basket.StoreBasket;
+
+namespace BasketAPI.Basket.DeleteBasket
 {
-    public class DeleteBasketEndpoints
+
+   // public record DeleteBasketResult(string UserName);
+    public record DeleteBasketResponse(bool IsSuccess);
+
+
+    public class DeleteBasketEndpoints : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+
+                var result = await sender.Send(new DeleteBasketCommand(userName));
+
+                var response = result.Adapt<DeleteBasketResponse>();
+
+
+                return Results.Ok(response);
+
+            })
+              .WithName("DeleteProduct")
+              .Produces<DeleteBasketResponse>(StatusCodes.Status201Created)
+              .ProducesProblem(StatusCodes.Status400BadRequest)
+              .ProducesProblem(StatusCodes.Status404NotFound)
+              .WithSummary("Delete Product")
+              .WithDescription("Delete Product");
+
+        }
     }
 }
--- src/Services/Basket/BasketAPI/Basket/DeleteBasket/ICommandHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BasketAPI.Basket.DeleteBasket
-{
-    public interface ICommandHandler<T1, T2, T3, T4>
-    {
-    }
-}
