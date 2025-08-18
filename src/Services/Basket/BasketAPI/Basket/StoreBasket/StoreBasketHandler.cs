56b5dac9238e53be711c37d25fe62874489ab195
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-08-18 23:25:41 +0600

    Change 111

--- src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,35 @@
-﻿namespace BasketAPI.Basket.StoreBasket
+﻿
+namespace BasketAPI.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(string userName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("cart cannot be null.");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required.");
+
+        }
+
     }
+
+    public class StoreBasketCommandHandler :
+        ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            ShoppingCart cart = command.Cart;
+
+
+            // Doto : store basket in database
+            // todo : update chace
+
+            return new StoreBasketResult("swn");
+
+        }
+    }
+
 }
