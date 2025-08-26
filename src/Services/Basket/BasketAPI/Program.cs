f2c43c28cc729a3338d58760ff8219247dba0879
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-08-26 20:50:27 +0600

    Change 138

--- src/Services/Basket/BasketAPI/Program.cs
+++ src/Services/Basket/BasketAPI/Program.cs
@@ -1,4 +1,4 @@
-using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,11 +27,21 @@ builder.Services.AddStackExchangeRedisCache( options =>
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    { 
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
 
 app.Run();
