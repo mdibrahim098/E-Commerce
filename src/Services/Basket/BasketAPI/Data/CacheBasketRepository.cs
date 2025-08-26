57c6fc831ec11bd3210d431fcee2a3d51934a38c
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-08-26 18:31:06 +0600

    Change 131

---
+++ src/Services/Basket/BasketAPI/Data/CacheBasketRepository.cs
@@ -0,0 +1,21 @@
+﻿
+namespace BasketAPI.Data
+{
+    public class CacheBasketRepository(IBasketRepository repositoiry) 
+        : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+        {
+           return await repositoiry.GetBasket(userName, cancellationToken);
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            return await repositoiry.StoreBasket(basket, cancellationToken);
+        }
+        public async Task<bool> DeleteBasket(string useName, CancellationToken cancellationToken = default)
+        {
+            return await repositoiry.DeleteBasket(useName, cancellationToken);
+        }
+    }
+}
