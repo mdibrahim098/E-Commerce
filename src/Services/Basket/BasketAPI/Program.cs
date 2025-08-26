e13e8af86f29279f86b272a3531404c9a0f55a73
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-08-26 19:56:14 +0600

    Change 132

--- src/Services/Basket/BasketAPI/Program.cs
+++ src/Services/Basket/BasketAPI/Program.cs
@@ -19,6 +19,11 @@ builder.Services.AddMarten( opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CacheBasketRepository>();
+builder.Services.AddStackExchangeRedisCache( options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
