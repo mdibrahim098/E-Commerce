a5966aa77248f5a6c7902934529246a4413c02fc
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-09-21 23:45:16 +0600

    Change 187

---
+++ src/Services/Ordering/OrderingApi/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿namespace OrderingApi
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            // app.Carter();
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            // app.Carter();
+
+            return app;
+        }
+
+    }
+}
--- src/Services/Ordering/OrderingApi/Program.cs
+++ src/Services/Ordering/OrderingApi/Program.cs
@@ -1,6 +1,16 @@
+using OrderingApi;
+using OrderingApplication;
+using OrderingInfrastrueture;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 
 var app = builder.Build();
 
