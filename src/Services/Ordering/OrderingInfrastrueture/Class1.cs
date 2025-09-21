b89759f5e80457110bbcb53eec8b59dd419dfbf6
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-09-21 23:45:16 +0600

    Change 187

--- src/Services/Ordering/OrderingInfrastrueture/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OrderingInfrastrueture
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Services/Ordering/OrderingInfrastrueture/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OrderingInfrastrueture
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+               IConfiguration configuration)
+        {
+
+            var configurationString = configuration.GetConnectionString("Dtabase");
+
+            //services.AddDbContext<OrderingContext>(options =>
+            //{
+            //    options.UseSqlServer(configurationString,
+            //        sqlOptions => sqlOptions.MigrationsAssembly(typeof(OrderingContext).Assembly.FullName));
+            //});
+
+            return services;
+
+        }
+    }
+ }
