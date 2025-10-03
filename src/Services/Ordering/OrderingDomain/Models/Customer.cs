ee0399cce1a3ccaae49e7cee65597d0e0be351cb
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-03 17:05:55 +0600

    Change 197

--- src/Services/Ordering/OrderingDomain/Models/Customer.cs
+++ src/Services/Ordering/OrderingDomain/Models/Customer.cs
@@ -4,7 +4,8 @@
     {
         public string Name { get; private set; } = default!;
         public string Email { get; private set; } = default!;
-        public static Customer Create( CustomerId id, string name, String email)
+        public static Customer Create( CustomerId id, string name,
+                                       string email)
         {
             ArgumentException.ThrowIfNullOrEmpty(name);
             ArgumentException.ThrowIfNullOrEmpty(email);
