684108d2e40b51c602196c197e0e15a07b9aebd1
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-03 17:40:22 +0600

    Change 199

--- src/Services/Ordering/OrderingDomain/GlobalUsing.cs
+++ src/Services/Ordering/OrderingDomain/GlobalUsing.cs
@@ -3,4 +3,6 @@ global using OrderingDomain.Enums;
 global using OrderingDomain.ValueObjects;
 global using MediatR;
 global using OrderingDomain.Exceptions;
+global using OrderingDomain.Events;
+global using OrderingDomain.Models;
 
