1d42dd12c690aeb2042bd179a2a2b67d0087d811
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-03 18:08:15 +0600

    Change 204

---
+++ src/Services/Ordering/OrderingInfrastrueture/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using OrderingDomain.Models;
+
+namespace OrderingInfrastrueture.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+
+
+}
