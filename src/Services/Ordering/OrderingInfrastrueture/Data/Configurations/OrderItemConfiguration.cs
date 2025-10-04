3a496bfbb7c2770c741dedf4a470b6ac77733b67
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-04 21:31:59 +0600

    Change 206

---
+++ src/Services/Ordering/OrderingInfrastrueture/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderingDomain.Models;
+
+namespace OrderingInfrastrueture.Data.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
