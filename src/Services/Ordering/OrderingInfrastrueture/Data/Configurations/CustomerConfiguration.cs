d5bb795d6c73f0ea18e5ff63ed4c53cee66d0075
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-04 22:38:31 +0600

    Change 207

--- src/Services/Ordering/OrderingInfrastrueture/Data/Configurations/CustomerConfiguration.cs
+++ src/Services/Ordering/OrderingInfrastrueture/Data/Configurations/CustomerConfiguration.cs
@@ -12,7 +12,7 @@ namespace OrderingInfrastrueture.Data.Configurations
         {
            builder.HasKey(c => c.Id);
            builder.Property(c => c.Id).HasConversion(
-                       CustomerId => CustomerId.Value,
+                       customerId => customerId.Value,
                        dbId => CustomerId.Of(dbId));
            builder.Property(c => c.Name).HasMaxLength(100).IsRequired();
            builder.Property(c => c.Email).HasMaxLength(255);
--- src/Services/Ordering/OrderingInfrastrueture/Data/Configurations/ProductConfiguration.cs
+++ src/Services/Ordering/OrderingInfrastrueture/Data/Configurations/ProductConfiguration.cs
@@ -13,7 +13,7 @@ namespace OrderingInfrastrueture.Data.Configurations
         {
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Id).HasConversion(
-                        ProductId => ProductId.Value,
+                        productId => productId.Value,
                         dbId => ProductId.Of(dbId));
             builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
 
