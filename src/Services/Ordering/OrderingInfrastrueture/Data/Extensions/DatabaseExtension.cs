52e91e11c54d7fabcaba1697510ae1414581c4bc
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-06 01:55:57 +0600

    Change 215

---
+++ src/Services/Ordering/OrderingInfrastrueture/Data/Extensions/DatabaseExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OrderingInfrastrueture.Data.Extensions
+{
+    public static class DatabaseExtension
+    {
+        public static async Task InitialiseDatabaseAsync( this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            context.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        }
+
+    }
+}
