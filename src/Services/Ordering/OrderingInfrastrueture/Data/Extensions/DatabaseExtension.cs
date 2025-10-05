d7a078d9971dc9507ca27fd29f27c73e94a0437f
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-06 02:07:57 +0600

    Change 216

--- src/Services/Ordering/OrderingInfrastrueture/Data/Extensions/DatabaseExtension.cs
+++ src/Services/Ordering/OrderingInfrastrueture/Data/Extensions/DatabaseExtension.cs
@@ -11,7 +11,23 @@ namespace OrderingInfrastrueture.Data.Extensions
             var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             context.Database.MigrateAsync().GetAwaiter().GetResult();
 
+            await SeedAsync(context);
         }
+        private static async Task SeedAsync(ApplicationDbContext context)
+        {
+            await SeedCustomersAsync(context);
+            //await SeedProductAsync(context);
+            //await SeedOrderAndItemsAsync(context);
+        }
+        private static async Task SeedCustomersAsync(ApplicationDbContext context)
+        {
+            if(!await context.Customers.AnyAsync())
+            {
+                await context.Customers.AddRangeAsync(InitialData.Customers);
+                await context.SaveChangesAsync();
+            }
+        }
+
 
     }
 }
