539482438d988aa5083aeda12455eab1b748c958
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-06 01:55:57 +0600

    Change 215

--- src/Services/Ordering/OrderingApi/Program.cs
+++ src/Services/Ordering/OrderingApi/Program.cs
@@ -1,6 +1,7 @@
 using OrderingApi;
 using OrderingApplication;
 using OrderingInfrastrueture;
+using OrderingInfrastrueture.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,5 +16,12 @@ builder.Services
 var app = builder.Build();
 
 // Congigure the http request pipeline.
+
+app.UseApiServices();
+
+if(app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
