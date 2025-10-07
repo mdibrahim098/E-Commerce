74e7638c7e7ca942492e567b0cedaea4be4eb053
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-08 03:00:57 +0600

    Change 235

--- src/Services/Ordering/OrderingInfrastrueture/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/OrderingInfrastrueture/Data/ApplicationDbContext.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
-using Microsoft.EntityFrameworkCore;
-using OrderingDomain.Models;
+using OrderingApplication.Data;
+
 
 namespace OrderingInfrastrueture.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext :DbContext, IApplicationDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options) { }
