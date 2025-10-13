0861e56595e4ca38374163cdb98d092f1089027e
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-13 21:22:07 +0600

    Change 236

--- src/Services/Ordering/OrderingApplication/GlobalUsing.cs
+++ src/Services/Ordering/OrderingApplication/GlobalUsing.cs
@@ -1,1 +1,6 @@
-﻿
+﻿global using BuildingBlocks.CQRS;
+global using OrderingApplication.Data;
+global using OrderingApplication.Dtos;
+global using OrderingDomain.Models;
+global using OrderingDomain.ValueObjects;
+
