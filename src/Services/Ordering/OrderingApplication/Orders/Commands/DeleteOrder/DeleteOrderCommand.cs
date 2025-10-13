ab17c31802b189d1e71562cdb0ded85c44fee76e
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-10-13 22:51:14 +0600

    Change 238

---
+++ src/Services/Ordering/OrderingApplication/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace OrderingApplication.Orders.Commands.DeleteOrder
+{
+
+    public record DeleteOrderCommand(Guid OrderId)
+        :ICommand<DeleteOrderResult>;
+
+    public record DeleteOrderResult(bool IsSuccess);
+
+
+    public class DleteOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public DleteOrderCommandValidator()
+        {
+            RuleFor(x => x.OrderId).NotEmpty().WithMessage("OrderId is required.");
+        }
+
+
+    }
+
+}
