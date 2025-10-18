21c8441983b4c2fd713e774c65f8f946ec953882
mdibrahim098 <67840334+mdibrahim098@users.noreply.github.com>
2025-10-18 19:43:44 +0600

    Change 252

--- src/Services/Ordering/OrderingApi/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/OrderingApi/Endpoints/UpdateOrder.cs
@@ -1,4 +1,7 @@
-﻿namespace OrderingApi.Endpoints
+﻿
+using OrderingApplication.Orders.Commands.UpdateOrder;
+
+namespace OrderingApi.Endpoints
 {
 
     // Accept a updateorderRequest
@@ -6,7 +9,34 @@
     // Sends the command for processing
     // return a success or error response basec on the outcome
 
-    public class UpdateOrder
+    public record UpdateOrderRequest(OrderDto order);
+    public record UpdateOrderResponse(bool IsSuccess);
+
+    public class UpdateOrder : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+
+            app.MapPost("/orders", async (UpdateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateOrderResponse>();
+
+                return Results.Ok(response);
+
+            })
+              .WithName("UpdateOrder")
+              .Produces<UpdateOrderResponse>(StatusCodes.Status200OK)
+              .ProducesProblem(StatusCodes.Status400BadRequest)
+              .WithSummary("Update Order")
+              .WithDescription("Update Order");
+
+        }
     }
+
+
+
 }
